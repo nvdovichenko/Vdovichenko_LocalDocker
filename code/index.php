3265887a38ce7750dbdd5abd3b3a1944b8d5781f
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-11 16:50:58 +0200

    #16 Функции

--- code/index.php
+++ code/index.php
@@ -127,3 +127,54 @@ function printStringReturnNumber(string $string):int
 $my_num = printStringReturnNumber('is worked');
 echo $my_num;
 
+// task 16
+echo "<br><br>#16<br>";
+
+function increaseEnthusiasm(string $string):string
+{
+    $string.='!';
+    return $string;
+}
+echo increaseEnthusiasm('Hello world')."<br>";
+
+function repeatThreeTimes(string $string):string
+{
+    $string.=$string.$string;
+    return $string;
+}
+echo repeatThreeTimes(increaseEnthusiasm(' repeat Hello'))."<br>";
+
+function cut(string $string, int $int=10):string
+{
+    $str = "";
+    for($i = 0; $i < $int; $i++)
+        $str.=$string[$i];
+    return $str;
+}
+echo cut("My name is PHP! How are you?",11)."<br>";
+
+function arrPrint($arr, $num)
+{
+    if ($num < sizeof($arr))
+    {
+        echo $arr[$num]." ";
+        arrPrint($arr, $num+1);
+    }
+}
+$array = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+arrPrint ($array, 0);
+
+function arrSum($num)
+{
+    $sum = 0;
+    while ($num > 0)
+    {
+        $sum+=$num%10;
+        $num/=10;
+    }
+    if ($sum > 9)
+        return arrSum($sum);
+    else
+        return($sum);
+}
+echo "<br>".arrSum(12345)."<br>";
