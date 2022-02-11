780c286953b877a769ef253b5ecd8bf561120214
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-11 15:29:13 +0200

    #12 Степень & correct code

--- code/index.php
+++ code/index.php
@@ -5,12 +5,10 @@ echo "#1";
 $very_bad_unclear_name = " 15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= " and a glass of juice";
-echo "<br>Your order is: $very_bad_unclear_name.";
+echo "<br>Your order is: $very_bad_unclear_name.<br>";
 
 // 2 task
-echo "<br><br>#2";
-echo "<br>";
-
+echo "<br>#2<br>";
 $num = 123;
 echo $num."<br>";
 $temperature = 36.6;
@@ -18,17 +16,19 @@ echo $temperature."<br>";
 echo ($num - 3*$temperature - 1.2)."<br>";
 $lastMonth = 1187.23;
 $thisMonth = 1089.98;
-echo "<br>Разница расходов = ";
+echo "Разница расходов = ";
 echo ($lastMonth-$thisMonth)."<br>";
 
 // 11 task
-echo "<br>#11";
-echo "<br>";
+echo "<br>#11<br>";
 $num_languages = 4;
 $months = 11;
 $days = $months * 16 / 4;
 $daysPerLanguages = $days;
-echo "<br>В среднем на один язык ушло дней: ";
+echo "В среднем на один язык ушло дней: ";
 echo $daysPerLanguages."<br>";
 
-
+// 12 task
+echo "<br>#12<br>";
+echo "8 в степени 2 = ";
+echo 8**2;
