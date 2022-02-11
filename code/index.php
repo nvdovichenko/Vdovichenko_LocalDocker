ef076f6b3e80de34c354ca6f5ef858fbcf977a23
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-11 15:15:32 +0200

    update code

--- code/index.php
+++ code/index.php
@@ -1,14 +1,11 @@
 <?php
+
 // 1 task
 echo "#1";
 /* Imagine a lot of code here */
 $very_bad_unclear_name = " 15 chicken wings";
-
-// Write your code here:
 $order = &$very_bad_unclear_name;
 $order .= " and a glass of juice";
-
-// Don't change the line below
 echo "<br>Your order is: $very_bad_unclear_name.";
 
 // 2 task
