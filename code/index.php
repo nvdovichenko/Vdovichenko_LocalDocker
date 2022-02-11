cdc9b42ebd9319737c9bee9fdeaf3cb9725f87f8
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-11 15:13:30 +0200

    #2 Числа

--- code/index.php
+++ code/index.php
@@ -1,10 +1,26 @@
 <?php
+// 1 task
+echo "#1";
 /* Imagine a lot of code here */
-$very_bad_unclear_name="15 chicken wings";
+$very_bad_unclear_name = " 15 chicken wings";
 
 // Write your code here:
 $order = &$very_bad_unclear_name;
-$order .=" and a glass of juice";
+$order .= " and a glass of juice";
 
 // Don't change the line below
-echo"\nYour order is:$very_bad_unclear_name.";
+echo "<br>Your order is: $very_bad_unclear_name.";
+
+// 2 task
+echo "<br><br>#2";
+echo "<br>";
+
+$num = 123;
+echo $num."<br>";
+$temperature = 36.6;
+echo $temperature."<br>";
+echo ($num - 3*$temperature - 1.2)."<br>";
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "<br>Разница расходов = ";
+echo ($lastMonth-$thisMonth)."<br>";
