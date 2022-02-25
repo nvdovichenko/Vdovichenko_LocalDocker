cabecc56d0440e6609fe890785bd93f47a7d0337
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-25 18:54:00 +0200

    #2(a)

--- code/one.php
+++ code/one.php
@@ -14,3 +14,42 @@ echo "<br><br>";
 $type = "/(\d)+/";
 $str = "a1b2c3";
 echo preg_replace_callback($type, fn($answer) => intval($answer[0]) ** 3, $str);
+echo "<br><br>";
+
+
+// task 2(a)
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>reg exp and file work</title>
+    </head>
+    <body>
+    <form method="POST">
+        <label>
+<textarea name="textToParse"
+          placeholder="Input English text to check"
+          rows="10" cols="50"></textarea>
+        </label>
+        <input type="submit" name="getWordsAndSymbols" value="text treatment ">
+    </form>
+    </body>
+    </html>
+
+<?php
+if ($_POST['getWordsAndSymbols']) {
+    if ($_POST['textToParse']) {
+        $_SESSION['textInfo'] = str_word_count($_POST['textToParse'],
+                0) . ' Words<br>';
+
+        $_SESSION['textInfo'] .= strlen($_POST['textToParse']) . ' Symbols<br>';
+
+    } else {
+        $_SESSION['textInfo'] = 'Without text! Input something';
+    }
+}
+echo $_SESSION['textInfo'];
