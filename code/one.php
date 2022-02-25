08d09e7aff3822cf416d3f336415e4cdb4164200
Nikita Vdovichenko <78236029+nvdovichenko@users.noreply.github.com>
2022-02-25 17:32:41 +0200

    #1(a-b) Регулярные выражения

--- code/one.php
+++ code/one.php
@@ -1,2 +1,16 @@
 <?php
-echo phpinfo();
+
+// Lab #3 for Web BFU
+
+// task 1(a)
+$type = "/a..b/";
+$str = "ahb acb aeb aeeb adcb axeb";
+$answer = [];
+preg_match_all($type, $str, $answer);
+print_r($answer);
+echo "<br><br>";
+
+// task 1(b)
+$type = "/(\d)+/";
+$str = "a1b2c3";
+echo preg_replace_callback($type, fn($answer) => intval($answer[0]) ** 3, $str);
